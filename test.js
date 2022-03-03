fd4d2f1892c0fcb957dac3216627dc3d3f611726
JohanLindberg <johan.lindberg@futuregames.nu>
2022-03-03 14:28:47 +0100

    commit

---
+++ test.js
@@ -0,0 +1,14 @@
+import * as THREE from 'three';
+
+const renderer = new THREE.WebGLRenderer({alpha: true});
+
+renderer.setClearColor(new THREE.Color('lightgrey'), 0.1);
+renderer.domElement.style.position = 'absolute';
+renderer.domElement.style.top = '0px';
+renderer.domElement.style.left = '0px';
+
+document.body.appendChild( renderer.domElement );
+
+const scene = new THREE.Scene();
+
+
