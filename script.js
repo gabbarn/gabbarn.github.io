0680446ab72fd0f49374fdef007db255cfcdc4eb
JohanLindberg <johan.lindberg@futuregames.nu>
2022-03-04 12:01:20 +0100

    as

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
        {
            name: 'Magnemite',
            location: {
-               lat: 44.496470,
-               lng: 11.320180,
+               lat: 64.749492,
+               lng: 20.959880,
            }
        },
    ];
