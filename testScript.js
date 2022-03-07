6c811de80cf4ef0c1542828dd2721611bcc15931
JohanLindberg <johan.lindberg@futuregames.nu>
2022-03-07 14:49:22 +0100

    as

---
+++ testScript.js
@@ -0,0 +1,13 @@
+
+
+function slaEnTarning(){
+    var slumpa = Math.floor(Math.random()* 6) + 1;
+    
+    document.querySelector(".hej").innerHTML += "<div id='tarning' onclick='valjTarning()'>" + slumpa + "</div>";
+}
+
+function valjTarning(){
+
+    navig
+    console.log(); 
+}
