867611deedfaee36ee2d9d611f4845f7646b89c8
JohanLindberg <johan.lindberg@futuregames.nu>
2022-03-15 09:56:44 +0100

    Update ComponentRegistrator.js

--- ComponentRegistrator.js
+++ ComponentRegistrator.js
@@ -1,48 +1,16 @@
-AFRAME.registerComponent('box-thing', {
-    schema: { // this is kinda like a constructor where u can put data into the component and the does stuff to the insides of the component
-        width: {type: 'number', default: 1},
-        height: {type: 'number', default: 1},
-        depth: {type: 'number', default: 1},
-        color: {type: 'color', default: '#AAA'}
-    },
-
-    init: function () {
-        var data = this.data;
-        var el = this.el;
-        this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
-        this.material = new THREE.MeshStandardMaterial({color: data.color});
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        el.setObject3D('mesh', this.mesh);
-      // Do something when component first attached.
-    },
-
-    update: function () {
-      // Do something when component's data is updated.
-    },
-
-    remove: function () {
-      // Do something the component or its entity is detached.
-    },
-
-    tick: function (time, timeDelta) {
-      // Do something on every scene tick or frame.
+AFRAME.registerComponent('look-at', {
+    schema: { type: 'selector' },
+    init: function () { },
+    tick: function () {
+        this.el.object3D.lookAt(this.data.object3D.position);
     }
 });
-AFRAME.registerComponent('look-at', {
-  schema: { type: 'selector' },
-
-  init: function () {},
-
-  tick: function () {
-    this.el.object3D.lookAt(this.data.object3D.position)
-  }
-});
-AFRAME.registerComponent('direct-towards-latlng',{
-  schema: {
-    type: 'selector'
-  },
-  init: function () {},
-  tick: function () {
-    let direction = this.data.object3D.position - this.el.object3D.position;
-  }
+AFRAME.registerComponent('direct-towards-latlng', {
+    schema: {
+        type: 'selector'
+    },
+    init: function () { },
+    tick: function () {
+        var direction = this.data.object3D.position.sub(this.el.object3D.position);
+    }
 });
